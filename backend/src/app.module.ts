a3ef1d9f4ecbcf0e733beb87d14fb6bb5e99b6a9
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-13 20:39:26 +0500

    feat: подключить модуль комплектующих

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { PrismaModule  } from './prisma/prisma.module.js';
-import { AuthModule } from './auth/auth.module.js'
-import { PcBuildsModule } from './pc-builds/pc-builds.module.js'
-import { LeadsModule } from './leads/leads.module.js'
-import { ConfiguratorModule } from './configurator/configurator.module.js'
-
+import { PrismaModule } from './prisma/prisma.module.js';
+import { AuthModule } from './auth/auth.module.js';
+import { PcBuildsModule } from './pc-builds/pc-builds.module.js';
+import { LeadsModule } from './leads/leads.module.js';
+import { ConfiguratorModule } from './configurator/configurator.module.js';
+import { ComponentsModule } from './components/components.module.js';
 
 @Module({
-  imports: [PrismaModule, AuthModule, PcBuildsModule, LeadsModule, ConfiguratorModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    PcBuildsModule,
+    LeadsModule,
+    ConfiguratorModule,
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
- 
