4ebebcdb6ed9324198f2b9ac9ca369ad2e96510a
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-13 20:57:58 +0500

    feat: register PC build admin module

--- backend/src/pc-builds/pc-builds.module.ts
+++ backend/src/pc-builds/pc-builds.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PcBuildsController } from './pc-builds.controller.js';
 import { PcBuildsService } from './pc-builds.service.js';
+import { PcBuildsAdminController } from './pc-builds-admin.controller.js';
+import { PcBuildsAdminService } from './pc-builds-admin.service.js';
+
 @Module({
-  controllers: [PcBuildsController],
-  providers: [PcBuildsService],
+  controllers: [PcBuildsController, PcBuildsAdminController],
+  providers: [PcBuildsService, PcBuildsAdminService],
 })
 export class PcBuildsModule {}
