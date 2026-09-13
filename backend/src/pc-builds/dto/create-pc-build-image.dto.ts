c47ee48da23ca148805c2d49d46f025afd703dd9
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-13 20:59:25 +0500

    fix: remove unused image DTO import

--- backend/src/pc-builds/dto/create-pc-build-image.dto.ts
+++ backend/src/pc-builds/dto/create-pc-build-image.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, IsString, IsUrl, Min } from 'class-validator';
+import { IsInt, IsOptional, IsUrl, Min } from 'class-validator';
 
 export class CreatePcBuildImageDto {
   @IsUrl()
