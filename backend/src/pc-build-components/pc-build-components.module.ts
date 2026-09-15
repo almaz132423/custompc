2e707171df01439fbc328ef6c13a689cecfb89fe
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-15 09:39:37 +0500

    fix: подключить AuthModule в PcBuildComponentsModule

--- backend/src/pc-build-components/pc-build-components.module.ts
+++ backend/src/pc-build-components/pc-build-components.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module.js';
 import { PcBuildComponentsController } from './pc-build-components.controller.js';
 import { PcBuildComponentsService } from './pc-build-components.service.js';
 import { CompatibilityService } from './compatibility.service.js';
@@ -6,6 +7,7 @@ import { ConfiguratorController } from './configurator.controller.js';
 import { ConfiguratorService } from './configurator.service.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PcBuildComponentsController, ConfiguratorController],
   providers: [PcBuildComponentsService, CompatibilityService, ConfiguratorService],
 })
