d0e75f2ecb199c5e5b9aeb7eb1500ae5ef0a55da
Алмазик Галиев < @nmt.local>
2026-09-15 09:25:19 +0500

    Revert "Merge branch 'main' of https://github.com/almaz132423/custompc"
    This reverts commit 0e9f389144677cd3557000bded7192b5ece8ff07, reversing
    changes made to c27d4a0a59ddc6f36fbb777f8273881514730da8.

--- backend/src/pc-builds/pc-builds.module.ts
+++ backend/src/pc-builds/pc-builds.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from '../auth/auth.module.js';
 import { PcBuildsController } from './pc-builds.controller.js';
 import { PcBuildsService } from './pc-builds.service.js';
 import { PcBuildsAdminController } from './pc-builds-admin.controller.js';
 import { PcBuildsAdminService } from './pc-builds-admin.service.js';
 
 @Module({
-  imports: [AuthModule],
   controllers: [PcBuildsController, PcBuildsAdminController],
   providers: [PcBuildsService, PcBuildsAdminService],
 })
