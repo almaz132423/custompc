2f6485d937db40f2af01a62e0731800409cd2205
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-18 14:48:59 +0500

    feat: register orders module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfiguratorModule } from './configurator/configurator.module.js';
 import { ComponentsModule } from './components/components.module.js';
 import { PcBuildComponentsModule } from './pc-build-components/pc-build-components.module.js';
 import { CompatibilityRulesModule } from './compatibility-rules/compatibility-rules.module.js';
+import { OrdersModule } from './orders/orders.module.js';
 
-@Module({ imports: [PrismaModule, AuthModule, PcBuildsModule, LeadsModule, ConfiguratorModule, ComponentsModule, PcBuildComponentsModule, CompatibilityRulesModule], controllers: [AppController], providers: [AppService] })
+@Module({ imports: [PrismaModule, AuthModule, PcBuildsModule, LeadsModule, ConfiguratorModule, ComponentsModule, PcBuildComponentsModule, CompatibilityRulesModule, OrdersModule], controllers: [AppController], providers: [AppService] })
 export class AppModule {}
