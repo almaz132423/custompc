ba06d20649ac0cfaa879bc0e3a0c4779f2baa961
almaz132423 <127094403+almaz132423@users.noreply.github.com>
2026-09-22 12:14:09 +0500

    feat: add reviews backend

---
+++ backend/src/reviews/reviews.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { ReviewsController } from './reviews.controller.js';
+import { ReviewsService } from './reviews.service.js';
+
+@Module({ controllers: [ReviewsController], providers: [ReviewsService], exports: [ReviewsService] })
+export class ReviewsModule {}
